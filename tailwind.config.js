5fc948657f8fe5b242cec4d1f799a4c73d16de64
Roosch <85234531+enzorooschqueiroz@users.noreply.github.com>
2025-01-18 22:34:35 -0300

    Update portfolio layout and styles; remove default content and add Hero component

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {
+      colors: {
+        black: {
+          DEFAULT: '#000',
+          100: "#000319"
+        }
+      }
+    },
+  },
+  plugins: [],
+}
+
