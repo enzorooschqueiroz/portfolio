07c619443fcab973254144d7b87ccffaabb1cdd1
Roosch <85234531+enzorooschqueiroz@users.noreply.github.com>
2025-01-19 00:25:04 -0300

    Update Hero component with padding and add title

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -2,8 +2,10 @@ import React from 'react'
 
 const Hero = () => {
   return (
-    <div>
-      
+    <div className='pb-20 pt-36 '>
+      <div>
+        <h1>Enzo da Gaby</h1>
+      </div>
     </div>
   )
 }
